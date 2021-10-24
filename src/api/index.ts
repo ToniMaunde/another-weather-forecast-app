985958ba0da7e53ef6a88d03d39a1eb50d2f9b0a
ToniMaunde <maundemilton@gmail.com>
2021-10-24 14:29:04 +0200

    fix: changed api method protocol

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 const requestHeaders: RequestInit = {method:'GET', mode: 'cors'}
 
 export const getCurrentWeatherInformation = async (city: string, apiKey: string, measurementSystem: string) => {
-  const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${measurementSystem}&appid=${apiKey}`
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${measurementSystem}&appid=${apiKey}`
 
   try {
     const result = await fetch(URL, requestHeaders)
