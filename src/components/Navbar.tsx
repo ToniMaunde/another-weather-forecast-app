89ce8c0c708560b83a08e7932a858316e3d983c7
Milton David <maundemilton@gmail.com>
2022-07-11 19:58:26 +0200

    style: move app icon from the left to the right

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,8 @@ export default function Navbar() {
   return (
     <nav className="py-4 px-7 flex items-center">
       <div className="flex text-secondary items-center">
-        <Icon {...logoIcon} customClasses="mr-1 flex-shrink-0 w-7 h-auto fill-primary"/>
         Weather&nbsp;<span className="font-semibold text-primary">App</span>
+        <Icon {...logoIcon} customClasses="ml-1 flex-shrink-0 w-7 h-auto fill-primary"/>
       </div>
       <ModeToggler classes="ml-auto mr-6 cursor-pointer" />
       <LanguageSwitcher />
